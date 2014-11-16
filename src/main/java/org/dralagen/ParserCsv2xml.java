d9d1e650e89bbee10a098c25416147abbb75257a
Dralagen <dralagen@dralagen.fr>
2014-11-16 09:14:24 +0100

    Add License AGPL for project. And complet pom information (scm, developer, name, license)

--- src/main/java/org/dralagen/ParserCsv2xml.java
+++ src/main/java/org/dralagen/ParserCsv2xml.java
@@ -1,5 +1,24 @@
 package org.dralagen;
 
+/*
+ * csv2xml
+ *
+ * Copyright (C) 2014 dralagen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -139,11 +158,11 @@ public class ParserCsv2xml {
             } finally {
                 try {
                     osw.close();
-                } catch (Exception e) {
+                } catch (Exception ignored) {
                 }
                 try {
                     baos.close();
-                } catch (Exception e) {
+                } catch (Exception ignored) {
                 }
             }
 
